ccf7e678460f5e13717665bc8103b31c5d3e2246
Christone007 <exellentemy@gmail.com>
2025-10-15 07:04:01 +0100

    Initialize project for control flow

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,7 +2,9 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5.0);
-    print_labeled_measurement(5, 'h');
+    print_labeled_measurement(5, 'a');
+    let sum = add_two_numbers(5, 6);
+    println!("The sum of 5 and 6 is: {sum}");
 }
 
 
@@ -12,4 +14,8 @@ fn another_function(x: f32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn add_two_numbers(x: i32, y: i32) -> i32 {
+    x + y
 }
