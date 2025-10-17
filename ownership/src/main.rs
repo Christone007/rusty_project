1528d8a4a740422dff28ad7d8900e91758f9c0d3
Christone007 <exellentemy@gmail.com>
2025-10-17 18:30:46 +0100

    See how ownership moves

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let s1 = "hello";
-    let s2 = s1;
+    let s1 = String::from("Emeka");
+    let s2 = 12;
 
-    println!("{s1} and {s2}");
+    my_fun1(s1); // ownership of the string is moved from s1 to my_func
+    my_fun2(7);
+   
+    println!("trying to access s1:- {}", s1);
+    println!("trying to access s2:- {}", s2);
+
+}
+
+fn my_fun1(x: String) {
+    println!("my_fun1 prints -> {x}");
+}
+
+fn my_fun2(x: i32) {
+    println!("my_fun2 prints -> {x}");
 }
