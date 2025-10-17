d3cdcf73d62d89e42e45a276adcfe682bbe6e280
Christone007 <exellentemy@gmail.com>
2025-10-17 18:33:22 +0100

    Ownership transfer differences

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     let s1 = String::from("Emeka");
     let s2 = 12;
 
-    my_fun1(s1); // ownership of the string is moved from s1 to my_func
-    my_fun2(7);
+    my_fun1(s1.clone()); // ownership of the string is moved from s1 to my_func, hence value has to be cloned to avoid dropping s1
+    my_fun2(s2); // no cloning needed as s2 does not really Own the value
    
     println!("trying to access s1:- {}", s1);
     println!("trying to access s2:- {}", s2);
