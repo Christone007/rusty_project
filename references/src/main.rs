3511c519b966995ea2aa7ddf7fb5907c2fcf091b
Christone007 <exellentemy@gmail.com>
2025-10-19 11:16:52 +0100

    Multiple immutable references are allowed

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let mut s1 = String::from("Hello");
+    let s1 = String::from("Hello");
 
-    {
-        let r1 = &mut s1;
-    }
-    let r2 = &mut s1;
+    let r1 = &s1;
+    let r2 = &s1;
 
-    println!("r1 = xx\nr2 = {}", r2);
+    println!("r1 = {}\nr2 = {}",r1, r2);
 }
