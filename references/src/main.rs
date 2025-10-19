5d362cf3909453c034344468e2443850ec6b3444
Christone007 <exellentemy@gmail.com>
2025-10-19 11:12:27 +0100

    Try multiple simultaneous borrowing

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
     let mut s1 = String::from("Hello");
 
-    change(&mut s1);
+    {
+        let r1 = &mut s1;
+    }
+    let r2 = &mut s1;
 
-    println!("The s1 value is {s1}");
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    println!("r1 = xx\nr2 = {}", r2);
 }
