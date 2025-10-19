68db846b01a1bcb4205f4650e79388154fa7a15b
Christone007 <exellentemy@gmail.com>
2025-10-19 10:55:43 +0100

    Use mutable references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let s1 = String::from("Hello");
+    let mut s1 = String::from("Hello");
 
-    let len = calculate_length(&s1);
+    change(&mut s1);
 
-    println!("The length of '{s1}' is {len}.");
+    println!("The s1 value is {s1}");
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
