9f7bf9e00231d1081198c3a5bea7ecf5048ef32c
Christone007 <exellentemy@gmail.com>
2025-10-25 10:58:51 +0100

    Complete stucts chapter

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,20 +1,37 @@
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    height: u32
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn can_hold(&self, rect2: &Self) -> bool {
+        self.area() > rect2.area()
+    }
+
 }
 
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    println!("rect1 is {rect1:?}");
-    dbg!(&rect1);
-}
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
 }
