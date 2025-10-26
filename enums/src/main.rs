f0d638431e46e4ded02495b2fdd74459a5efc270
Christone007 <exellentemy@gmail.com>
2025-10-26 15:05:54 +0100

    Print amount value of US coins

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,42 @@
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    California,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-
-    #[derive(Debug)]
-    enum Message {
-        Quit,
-        Move { x: i32, y: i32},
-        Write(String),
-        ChangeColor(i32, i32, i32),
-    }
-
     let m1 = Message::Write(String::from("This is a message"));
 
     println!("{:#?}", m1);
+
+    let value = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("The value of the coin is {value} cents");
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
 }
